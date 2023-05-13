819212fc5ecf3a1b1158165f79f733d77228a124
Tunji Oyeniran <oyenirantunji2339@gmail.com>
2023-05-13 04:11:48 -0400

    ADD : simple page transition

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
-import { useEffect, useCallback } from "react";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import { page, setColorScheme } from "@/store/page";
+import { PageTransition } from "next-page-transitions";
 
 import "@picocss/pico";
 import "src/style/theme.css";
 import "src/style/style.css";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const { pathname } = useRouter();
   const { colorScheme } = page.use();
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", colorScheme);
@@ -28,8 +31,29 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
-      <Component {...pageProps} />
+      <PageTransition timeout={300} classNames="main page-transition">
+        <Component {...pageProps} key={pathname} />
+      </PageTransition>
       <Footer />
+      <style jsx global>{`
+        .main {
+          flex: 1;
+        }
+        .page-transition-enter {
+          opacity: 0;
+        }
+        .page-transition-enter-active {
+          opacity: 1;
+          transition: opacity 300ms;
+        }
+        .page-transition-exit {
+          opacity: 1;
+        }
+        .page-transition-exit-active {
+          opacity: 0;
+          transition: opacity 300ms;
+        }
+      `}</style>
     </>
   );
 }
